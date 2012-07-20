ee28f89fe94e1d84e50720b46990a753a12628aa
visshal <visshal.mehta@gmail.com>
2012-07-21 02:12:20 +0530

    Code tested, fragmented in function

--- untar_con.sh
+++ untar_con.sh
@@ -1,56 +1,82 @@
-#!/bin/sh
+#!/bin/bash
 ############################################################################
 # README:
 # This script extracts (n) tarballs concurrently and removes the tarball
 # once untar is successful. Upon even a single extraction fails script
 # errors out.
 #
-# Usage:
-# ./untar_con.sh <Path to tarballs dir>
-#
 # Date:
 # 07/18/2012 11:42:43
 # 
-# Note:
-# Part of Inktank Interview Questions.
+# Example:
+# ./untar_con.sh <press enter>
+# Enter the path of tar-balls: abc
+# Enter the path for extraction: newuntar
+#
+# Note: This script takes a path where all the tar-balls are residing.
+#       And extracts all the tarballs in the extraction path provided.
+#       Please provide absolute paths.
 #
 # Author:
 # Vishal Mehta
 #
-# Known Bug:
-# 'rm -f filename' always returns true, hence verification of this command,
-# should be done by 'ls filename'.
 ############################################################################
 
-# Array that holds processes.
-file_arr=[]
-# Array that holds processes.
-proc_arr=[]
-# Scalar variable that keeps count of bg processes.
-proc_count=0
-# Get tarball from listing given dir.
-for file in `ls`
-do
-   echo $file
-   # Running bg command.
-   a=`tar -zxvf $file &`
-   proc_arr[$proc_count]=$!
-   file_arr[$proc_count]=$file
-   proc_count=`expr $proc_count + 1`
-   echo $a
-done
 
-for (( i=0; i<proc_count; i++ ))
-do
-   echo ${proc_arr[$i]}
-   # Waiting for each bg process to complete.
-   if wait ${proc_arr[$i]} && `rm -f ${file_arr[$i]}`
-   then
-     echo "Hurray we passed."
-   else
-     echo "That failed :(."
-     exit 1
-   fi
-   echo $ret
-done
+# Function.
+untar()
+{
+  local tar_list=$1
+  local untar_dir=$2
+  echo "Untar called"
+  local c=0
+  local procarr=[]
+  for file in `ls $tar_path`
+  do
+    echo $file
+    tar -zxf $tar_path/$file -C $untar_dir/ &
+    procarr[$c]=$!
+    c=`expr $c + 1`
+  done
+
+  echo ${procarr[0]}
+  echo ${procarr[1]}
+  echo ${procarr[2]}
+  
+  # Wait for bg processes to be run.
+  for (( i=0; i<=c; i++ ))
+  do
+    echo "Waiting for process : ${procarr[$i]}"
+    if wait ${procarr[$i]}
+    then
+      echo "hurray we passed."
+    else
+      echo "That failed :("
+      exit 1
+    fi
+  done
+  return 0
+}
+
+# Get the path where all tarballs are kept.
+echo -e "Enter the abs path of tar-balls: \c"
+read tar_path
+if [ ! -d $tar_path ]
+then 
+  echo "$tar_path : No such path exist."
+  exit 1
+fi
+
+# Get the path where all the tarballs need to be extracted.
+echo -e "Enter the abs path for extraction: \c"
+read untar_path
+if [ ! -d $untar_path ]
+then 
+  echo "$untar_path : No such path exist."
+  exit 1
+fi
+
+# Calling untar function.
+untar $tar_path, $untar_path
+
 exit 0
